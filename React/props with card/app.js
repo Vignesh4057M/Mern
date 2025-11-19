b244a4e82ebbc7ace18a56beec738fe53740fd03
Vignesh4057M <146635402+Vignesh4057M@users.noreply.github.com>
2025-11-19 09:15:11 +0530

    Add ConditionRA component to App

---
+++ React/props with card/app.js
@@ -0,0 +1,22 @@
+
+// import Props from "./component/Props/Props";
+// import Card from "./component/Samplecards/Card";
+import ConditionRA from "./component/Usestate/ConditionRA";
+// import Listrender from "./component/Listrender/Listrender";
+
+ 
+function App() {
+  return (
+
+    <>
+    {/* <Card/>  */}
+     {/* <Props/> */}
+    <ConditionRA/>
+    {/* <Listrender/> */}
+    
+    
+    </>
+  );
+}
+
+export default App;
